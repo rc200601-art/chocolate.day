96da4c74adbdf7a0fe01a6431f02c2ec5c0ee9d0
rc200601-art <rc200601@gmail.com>
2026-02-08 23:02:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 const chocolate = document.getElementById("chocolate");
-const blocks = document.querySelectorAll(".blocks span");
+const blocks = document.querySelectorAll(".top span");
 const heart = document.getElementById("heart");
 const message = document.getElementById("message");
 const hint = document.getElementById("hint");
 
-let index = 0;
+let i = 0;
 
 chocolate.addEventListener("click", () => {
 
-  if (index < blocks.length) {
-    blocks[index].classList.add("eaten");
-    index++;
-
-    // chocolate shrink effect
-    chocolate.style.transform = `scale(${1 - index * 0.05})`;
+  if (i < blocks.length) {
+    blocks[i].classList.add("eaten");
+    chocolate.style.transform =
+      `rotateX(18deg) rotateY(-18deg) scale(${1 - i * 0.05})`;
+    i++;
   }
 
-  if (index === blocks.length) {
-    hint.style.display = "none";
+  if (i === blocks.length) {
     heart.classList.add("pop");
+    hint.style.display = "none";
 
     setTimeout(() => {
       chocolate.style.display = "none";
       message.classList.remove("hidden");
-    }, 600);
+    }, 700);
   }
 });
